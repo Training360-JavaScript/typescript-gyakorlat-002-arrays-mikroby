ed42b29a3e8d8572e819736d0801029eecf3bdce
mikroby <rtamas@hotmail.com>
2022-01-07 03:58:15 +0100

    solved

--- src/lesson-01.ts
+++ src/lesson-01.ts
@@ -3,9 +3,9 @@ export default class calculator {
     active: boolean = false;
 
     // Hozd létre a következő változókat a megfelelő típusannotációval.
-    
-    /** @type {Array} points - számok tömbje, 1-től 10-ig egyesével. */
 
+    /** @type {Array} points - számok tömbje, 1-től 10-ig egyesével. */
+    points: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     /** 
      * A tömb minden eleme legyen egy objektum, amely egy felhasználót reprezentál.
@@ -13,21 +13,29 @@ export default class calculator {
      * Inicializáld a tömböt, legyen legalább két eleme.
      * @type {Array} users - minden elem egy felhasználó.
      */
+    users: Array<{ name: string, email: string }> = [
+        { name: "Tökfej Berci", email: "tfb@gmail.com" },
+        { name: "John DoeDoe", email: "jd@gmail.com" }
+    ];
 
-    
     /** 
      * A tömb minden eleme legyen egy objektum, amely egy terméket reprezentál.
      * Kötelező elemek: name<string>, price<number>, stock<number>.
      * Opcionális elemek: active<boolean>.
      * Inicializáld a tömböt, legyen legalább két eleme.
      * @type {Array} products - minden elem egy termék.
-     */    
+     */
+    products: Array<{ name: string, price: number, stock: number, active?: boolean }> =
+        [
+            { name: "fejhallgató", price: 15000, stock: 10, active: true },
+            { name: "mikrofon", price: 3000, stock: 20, active: false },
+            { name: "monitor", price: 130000, stock: 30 },
+        ]
 
-    
     /** 
      * Inicializálj egy tuple típust, azaz adj neki értéket is.
      * @type {Tuple} role - number, string, boolean. 
      */
-
+    role: [number, string, boolean] = [14, "jó ez", true];
 
 }
